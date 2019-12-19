4eb300c20e57b1f7aceacfbc9cd1eb0e76a620a8
Chelsi Christmas <c.christmas95@gmail.com>
2019-12-19 17:21:23 -0500

    Complete. Unable to properly implement unit tests

---
+++ Elements/ElementsTests/ElementsTests.swift
@@ -0,0 +1,25 @@
+//
+//  ElementsTests.swift
+//  ElementsTests
+//
+//  Created by Chelsi Christmas on 12/19/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import XCTest
+
+
+//class ElementsTests: XCTestCase {
+//
+//    func testFavoriteElements() {
+//        let name = "Xenon"
+//        let atomic_mass = 0.0
+//        let number = 0
+//        let symbol = "Xe"
+//        let boil = 0
+//        let melt = 0
+//        let discovered_by = "Chelsi"
+//
+//
+//
+//}
